7d473f6f10d11b5029d91dedf939fbf5b81bb118
jiachen.zhang <zhangjc1999@gmail.com>
2021-12-05 21:48:37 +0800

    [Feature] add benchmark testing for bigcache storage. (2.5x faster on set than gocache)

---
+++ storage/bigcache/bigcache_storage_bench_test.go
@@ -0,0 +1,42 @@
+package bigcache
+
+import (
+	"fmt"
+	"github.com/allegro/bigcache/v3"
+	"math"
+	"testing"
+	"time"
+)
+
+func BenchmarkBigcacheSet_Bytes(b *testing.B) {
+	client, _ := bigcache.NewBigCache(bigcache.DefaultConfig(5 * time.Minute))
+	store := NewBigCacheStorage(client, nil)
+	for k := 0.; k <= 10; k++ {
+		n := int(math.Pow(2, k))
+		b.Run(fmt.Sprintf("%d", n), func(b *testing.B) {
+			for i := 0; i < b.N*n; i++ {
+				key := fmt.Sprintf("key-%d", n)
+				value := []byte(fmt.Sprintf("value-%d", n))
+				_ = store.Set(key, value)
+			}
+		})
+	}
+}
+
+func BenchmarkBigcacheGet(b *testing.B) {
+	client, _ := bigcache.NewBigCache(bigcache.DefaultConfig(5 * time.Minute))
+	store := NewBigCacheStorage(client, nil)
+
+	key := "test"
+	_ = store.Set(key, []byte("value"))
+
+	var value []byte
+	for k := 0.; k <= 10; k++ {
+		n := int(math.Pow(2, k))
+		b.Run(fmt.Sprintf("%d", n), func(b *testing.B) {
+			for i := 0; i < b.N*n; i++ {
+				_ = store.Get(key, &value)
+			}
+		})
+	}
+}
